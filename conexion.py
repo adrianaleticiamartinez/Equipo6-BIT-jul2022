7a267c9866dfd8313f74d0d599c1f523fda30ea1
José Luis Velázquez Rodríguez <joseluisvelazquezro@gmail.com>
2022-07-14 10:13:24 -0500

    add conexion

---
+++ conexion.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def getCliente(idCliente):
+    clientes = pd.read_csv('baseClientesHackaton2022.csv')
+    return clientes[clientes.idCliente==idCliente]
+
+def getUsuario(usuario):
+    usuarios = pd.read_csv('baseUsuarios.csv')
+    return usuarios[usuarios.usuario==usuario]
+
+print(getUsuario("ENM").perfil)
