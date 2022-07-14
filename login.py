3c27a20d036cc197c7e7c93333050b82f8dedabb
José Luis Velázquez Rodríguez <joseluisvelazquezro@gmail.com>
2022-07-14 11:41:28 -0500

    version 1.2

--- login.py
+++ login.py
@@ -19,7 +19,20 @@ print("Ingrese el id del cliente")
 idCliente=(str(input()))
 c = conexion.getCliente(idCliente)
 if u.perfil.astype("string").iloc[0] == "Manager":
-    print(c)
+    print("idCliente: " + c.idCliente.astype("string").iloc[0])
+    print("nombre: " + c.nombre.astype("string").iloc[0])
+    print("apellidoPaterno: " + c.apellidoPaterno.astype("string").iloc[0])
+    print("apellidoMaterno: " + c.apellidoMaterno.astype("string").iloc[0])
+    print("fechaNacimiento: " + c.fechaNacimiento.astype("string").iloc[0])
+    print("sexo: " + c.sexo.astype("string").iloc[0])
+    print("segmento: " + c.segmento.astype("string").iloc[0])
+    print("nacionalidad: " + c.nacionalidad.astype("string").iloc[0])
+    print("rfc: " + c.rfc.astype("string").iloc[0][0:3])
+    print("tipoID: " + c.tipoID.astype("string").iloc[0][0:3])
+    print("numeroID: " + c.numeroID.astype("string").iloc[0][0:3])
+    print("cuenta: " + c.cuenta.astype("string").iloc[0])
+    print("email: " + c.email.astype("string").iloc[0][0:3])
+
 elif u.perfil.astype("string").iloc[0] == "Validador":
     print("idCliente: " + c.idCliente.astype("string").iloc[0])
     print("nombre: " + c.nombre.astype("string").iloc[0])
@@ -34,6 +47,7 @@ elif u.perfil.astype("string").iloc[0] == "Validador":
     print("numeroID: " + c.numeroID.astype("string").iloc[0][0:3] + "***")
     print("cuenta: " + c.cuenta.astype("string").iloc[0])
     print("email: " + c.email.astype("string").iloc[0][0:3] + "***")
+
 else:
     print("idCliente: " + c.idCliente.astype("string").iloc[0])
     print("nombre: " + c.nombre.astype("string").iloc[0])
